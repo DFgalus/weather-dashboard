ab328f4b8a8ff0cc0d5e6f1d3aaa28d08195840c
Dgalus <damiangalus7@gmail.com>
2023-01-09 16:37:33 -0500

    Fixed typo in JS, line 22

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 function findCity() {
-    let cityName = $('#cityname')[0].value.trim()
+    let cityName = titleCase($("#cityName")[0].value.trim());
 
     let apiURL = "https://api.openweathermap.org/data/2.5/weather?=" + cityName + "&units=imperial&appid=85fdad770cb1b5590007071765f836b5";
     fetch(apiURL).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 $("#cityName")[0].textContent = cityName + "(" + dayjs().format('M/D/YYYY') + ")";
+
                 $("#city-list").append('<button type="button" class="list-group-item list-group-item-light list-group-item-action city-name">' + 
                 cityName);
 
@@ -18,7 +19,7 @@ function findCity() {
 
                 apiURL = "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly &appid=85fdad770cb1b5590007071765f836b5";
 
-                fetch(apiURL).then(funtion(newResponse) {
+                fetch(apiURL).then(function (newResponse) {
                     if (newResponse.ok) {
                         newResponse.json().then(function (newData) {
                             getCurrentWeather(newData);
@@ -27,7 +28,7 @@ function findCity() {
                 })
                 
             })
-        } else {
+        }  else {
             alert("Cannot find city!!");
         }
     })
